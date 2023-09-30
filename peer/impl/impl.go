7217d0c750d2c11a34c702fd702a6a0ffb1e7141
PhilipLGQ <guanqun.liu@epfl.ch>
2023-09-30 16:26:03 +0200

    hw0 local tests passed

--- peer/impl/impl.go
+++ peer/impl/impl.go
@@ -1,8 +1,16 @@
 package impl
 
 import (
+	"context"
+	"errors"
+	"github.com/rs/zerolog/log"
 	"go.dedis.ch/cs438/peer"
 	"go.dedis.ch/cs438/transport"
+	"go.dedis.ch/cs438/types"
+	"golang.org/x/xerrors"
+	"io"
+	"sync"
+	"time"
 )
 
 // NewPeer creates a new peer. You can change the content and location of this
@@ -10,7 +18,13 @@ import (
 func NewPeer(conf peer.Configuration) peer.Peer {
 	// here you must return a struct that implements the peer.Peer functions.
 	// Therefore, you are free to rename and change it as you want.
-	return &node{}
+
+	// Save node configs
+	node := node{conf: conf}
+	// Initialize the routing table
+	node.tbl = ConcurrentRT{RT: make(map[string]string)}
+	node.tbl.AddEntry(node.conf.Socket.GetAddress(), node.conf.Socket.GetAddress())
+	return &node
 }
 
 // node implements a peer to build a Peerster system
@@ -18,36 +32,196 @@ func NewPeer(conf peer.Configuration) peer.Peer {
 // - implements peer.Peer
 type node struct {
 	peer.Peer
-	// You probably want to keep the peer.Configuration on this struct:
-	//conf peer.Configuration
+	// Keep the peer.Configuration on this struct:
+	conf peer.Configuration
+	// Group of goroutines launched by the node
+	wg sync.WaitGroup
+	// Node running status indicator
+	cancel context.CancelFunc
+	ctx    context.Context
+	// Concurrent routing table
+	tbl ConcurrentRT
+}
+
+// ExecChatMessage: the handler. This function will be called when a chat message is received.
+func ExecChatMessage(msg types.Message, pkt transport.Packet) error {
+	// cast the message to its actual type. You assume it is the right type.
+	chatMsg, ok := msg.(*types.ChatMessage)
+	if !ok {
+		return xerrors.Errorf("wrong type: %T", msg)
+	}
+	log.Info().Msg(chatMsg.String())
+	return nil
 }
 
 // Start implements peer.Service
 func (n *node) Start() error {
-	panic("to be implemented in HW0")
+	/* panic("to be implemented in HW0") */
+	// Deal with possible error in gouroutine
+	chanError := make(chan error, 1)
+	// Create new context allowing the goroutine to know Stop() call
+	n.ctx, n.cancel = context.WithCancel(context.Background())
+
+	// Mark start of goroutine
+	n.wg.Add(1)
+	n.conf.MessageRegistry.RegisterMessageCallback(types.ChatMessage{}, ExecChatMessage)
+
+	go func(ctx context.Context, cs chan error) {
+		defer n.wg.Done()
+		for {
+			// Return if Stop() is called
+			select {
+			case <-ctx.Done():
+				return
+			default:
+			}
+			// Process when no Stop() is called
+			pkt, err := n.conf.Socket.Recv(time.Millisecond * 10)
+			if errors.Is(err, transport.TimeoutError(0)) {
+				continue
+			} else if err != nil {
+				cs <- err
+			}
+			// Check if message should be permitted to register
+			n.wg.Add(1)
+			go func(cs chan error) {
+				defer n.wg.Done()
+				err := n.ProcessMessage(pkt)
+				if err != nil {
+					cs <- err
+				}
+			}(chanError)
+		}
+	}(n.ctx, chanError)
+
+	select {
+	case cError := <-chanError:
+		return cError
+	default:
+		return nil
+	}
 }
 
 // Stop implements peer.Service
 func (n *node) Stop() error {
-	panic("to be implemented in HW0")
+	/* panic("to be implemented in HW0") */
+	// Inform all goroutine to stop
+	n.cancel()
+	// Wait for completion of all goroutines
+	n.wg.Wait()
+	return nil
 }
 
 // Unicast implements peer.Messaging
 func (n *node) Unicast(dest string, msg transport.Message) error {
-	panic("to be implemented in HW0")
+	/*panic("to be implemented in HW0")*/
+	header := transport.NewHeader(n.conf.Socket.GetAddress(), n.conf.Socket.GetAddress(), dest, 0)
+	packet := transport.Packet{Header: &header, Msg: &msg}
+	nHop, eHop := n.tbl.Check(dest)
+	if !eHop {
+		return xerrors.Errorf("Destination address unknown!")
+	}
+	return n.conf.Socket.Send(nHop, packet, time.Millisecond*10)
 }
 
 // AddPeer implements peer.Service
 func (n *node) AddPeer(addr ...string) {
-	panic("to be implemented in HW0")
+	/* panic("to be implemented in HW0") */
+	for _, address := range addr {
+		if n.conf.Socket.GetAddress() == address { // exclude adding the peer itself
+			continue
+		}
+		n.tbl.AddEntry(address, address)
+	}
 }
 
 // GetRoutingTable implements peer.Service
 func (n *node) GetRoutingTable() peer.RoutingTable {
-	panic("to be implemented in HW0")
+	/* panic("to be implemented in HW0") */
+	return n.tbl.GetTable()
 }
 
 // SetRoutingEntry implements peer.Service
 func (n *node) SetRoutingEntry(origin, relayAddr string) {
-	panic("to be implemented in HW0")
+	/* panic("to be implemented in HW0") */
+	if relayAddr == "" {
+		n.tbl.RemoveEntry(origin)
+	} else {
+		n.tbl.AddEntry(origin, relayAddr)
+	}
+}
+
+// ProcessMessage implements the permission check on received messages
+func (n *node) ProcessMessage(pkt transport.Packet) error {
+	// If the message is for this node: process
+	if pkt.Header.Destination == n.conf.Socket.GetAddress() {
+		// Register if no error
+		err := n.conf.MessageRegistry.ProcessPacket(pkt)
+		if err != nil {
+			log.Info().Msgf("Error: %v", err.Error())
+			return err
+		}
+	} else { // Relay the message to the next hop if exists
+		header := transport.NewHeader(pkt.Header.Source, n.conf.Socket.GetAddress(), pkt.Header.Destination, 0)
+		packet := transport.Packet{Header: &header, Msg: pkt.Msg}
+		nHop, eHop := n.tbl.Check(pkt.Header.Destination)
+		if !eHop {
+			return xerrors.Errorf("Destination address unknown!")
+		}
+		return n.conf.Socket.Send(nHop, packet, time.Millisecond*10)
+	}
+	return nil
+}
+
+// ConcurrentRT implements the concurrent routing table
+type ConcurrentRT struct {
+	RT peer.RoutingTable
+	mu sync.Mutex
+}
+
+// Check if the input address value has a next hop, return the value and existence
+func (rt *ConcurrentRT) Check(addr string) (string, bool) {
+	rt.mu.Lock()
+	defer rt.mu.Unlock()
+	nAddr, exist := rt.RT[addr]
+	return nAddr, exist
+}
+
+// AddEntry adds a routing entry (peer) to the routing table
+func (rt *ConcurrentRT) AddEntry(key, value string) {
+	rt.mu.Lock()
+	defer rt.mu.Unlock()
+	rt.RT[key] = value
+}
+
+// RemoveEntry removes a routing entry (peer) from the routing table
+func (rt *ConcurrentRT) RemoveEntry(key string) {
+	rt.mu.Lock()
+	defer rt.mu.Unlock()
+	delete(rt.RT, key)
+}
+
+// GetTable returns a current copy of the routing table
+func (rt *ConcurrentRT) GetTable() peer.RoutingTable {
+	rt.mu.Lock()
+	defer rt.mu.Unlock()
+	tblCopy := make(map[string]string)
+	for k, v := range rt.RT {
+		tblCopy[k] = v
+	}
+	return tblCopy
+}
+
+// String implements safe access to peer.RoutingTable.String()
+func (rt *ConcurrentRT) String() string {
+	rt.mu.Lock()
+	defer rt.mu.Unlock()
+	return rt.RT.String()
+}
+
+// DisplayGraph implements safe access to display the routing table in graphviz graph
+func (rt *ConcurrentRT) DisplayGraph(out io.Writer) {
+	rt.mu.Lock()
+	defer rt.mu.Unlock()
+	rt.RT.DisplayGraph(out)
 }
--- transport/udp/udp.go
+++ transport/udp/udp.go
@@ -1,9 +1,13 @@
 package udp
 
 import (
-	"time"
-
+	"errors"
 	"go.dedis.ch/cs438/transport"
+	"math"
+	"net"
+	"os"
+	"sync"
+	"time"
 )
 
 const bufSize = 65000
@@ -21,7 +25,16 @@ type UDP struct {
 
 // CreateSocket implements transport.Transport
 func (n *UDP) CreateSocket(address string) (transport.ClosableSocket, error) {
-	panic("to be implemented in HW0")
+	/* panic("to be implemented in HW0") */
+	udpAddr, err := net.ResolveUDPAddr("udp4", address)
+	if err != nil {
+		return &Socket{}, err
+	}
+	conn, err := net.ListenUDP("udp4", udpAddr)
+	if err != nil {
+		return &Socket{}, err
+	}
+	return &Socket{conn: *conn}, nil
 }
 
 // Socket implements a network socket using UDP.
@@ -29,38 +42,126 @@ func (n *UDP) CreateSocket(address string) (transport.ClosableSocket, error) {
 // - implements transport.Socket
 // - implements transport.ClosableSocket
 type Socket struct {
+	// UDP connection status
+	conn net.UDPConn
+	// Lists of send and received packets
+	sendlist PktList
+	recvlist PktList
+}
+
+// PktList implements a protected packet lists in concurrent R/W
+type PktList struct {
+	pkl []transport.Packet
+	mu  sync.RWMutex
 }
 
 // Close implements transport.Socket. It returns an error if already closed.
 func (s *Socket) Close() error {
-	panic("to be implemented in HW0")
+	/* panic("to be implemented in HW0") */
+	err := s.conn.Close()
+	return err
 }
 
 // Send implements transport.Socket
 func (s *Socket) Send(dest string, pkt transport.Packet, timeout time.Duration) error {
-	panic("to be implemented in HW0")
+	/* panic("to be implemented in HW0") */
+	udpAddr, err := net.ResolveUDPAddr("udp4", dest)
+	if err != nil {
+		return err
+	}
+	conn, err := net.DialUDP("udp4", nil, udpAddr)
+	if err != nil {
+		return err
+	}
+
+	// Regularize timeout if required
+	if timeout <= 0 {
+		timeout = math.MaxInt64
+	}
+	err = conn.SetWriteDeadline(time.Now().Add(timeout))
+	if err != nil {
+		return err
+	}
+
+	// Marshal packet
+	buf, err := pkt.Marshal()
+	if err != nil {
+		return err
+	}
+	_, errSend := conn.Write(buf)
+	if errSend != nil {
+		return errSend
+	}
+
+	// Add sent packet to sendlist
+	s.sendlist.AddPacket(pkt.Copy())
+	return nil
 }
 
 // Recv implements transport.Socket. It blocks until a packet is received, or
 // the timeout is reached. In the case the timeout is reached, return a
 // TimeoutErr.
 func (s *Socket) Recv(timeout time.Duration) (transport.Packet, error) {
-	panic("to be implemented in HW0")
+	/* panic("to be implemented in HW0") */
+	var pkt transport.Packet
+
+	if timeout > 0 {
+		err := s.conn.SetReadDeadline(time.Now().Add(timeout))
+		if err != nil {
+			return transport.Packet{}, err
+		}
+	}
+
+	buf := make([]byte, bufSize)
+	lenRecv, _, errRecv := s.conn.ReadFromUDP(buf)
+	if errors.Is(errRecv, os.ErrDeadlineExceeded) {
+		return transport.Packet{}, transport.TimeoutError(0)
+	} else if errRecv != nil {
+		return transport.Packet{}, errRecv
+	}
+	err := pkt.Unmarshal(buf[:lenRecv])
+	if err != nil {
+		return transport.Packet{}, err
+	}
+	s.recvlist.AddPacket(pkt.Copy())
+	return pkt, nil
 }
 
 // GetAddress implements transport.Socket. It returns the address assigned. Can
 // be useful in the case one provided a :0 address, which makes the system use a
 // random free port.
 func (s *Socket) GetAddress() string {
-	panic("to be implemented in HW0")
+	/* panic("to be implemented in HW0") */
+	return s.conn.LocalAddr().String()
 }
 
 // GetIns implements transport.Socket
 func (s *Socket) GetIns() []transport.Packet {
-	panic("to be implemented in HW0")
+	/* panic("to be implemented in HW0") */
+	return s.recvlist.GetList()
 }
 
 // GetOuts implements transport.Socket
 func (s *Socket) GetOuts() []transport.Packet {
-	panic("to be implemented in HW0")
+	/* panic("to be implemented in HW0") */
+	return s.sendlist.GetList()
+}
+
+// GetList implements PktList of returning a copy of the packet list
+func (pl *PktList) GetList() []transport.Packet {
+	pl.mu.RLock()
+	defer pl.mu.RUnlock()
+
+	pklCopy := make([]transport.Packet, len(pl.pkl))
+	for idx, pkt := range pl.pkl {
+		pklCopy[idx] = pkt.Copy()
+	}
+	return pklCopy
+}
+
+// AddPacket implements PktList of adding a packet to the packet list
+func (pl *PktList) AddPacket(pkt transport.Packet) {
+	pl.mu.RLock()
+	defer pl.mu.RUnlock()
+	pl.pkl = append(pl.pkl, pkt)
 }
